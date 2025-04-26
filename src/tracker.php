35842827060db690152fef5b7f15905658d1bfab
acidRain810747 <acidRain810747@protonmail.com>
2025-04-26 15:18:28 +0300

    improve performance

---
+++ src/tracker.php
@@ -0,0 +1,8 @@
+<?php
+
+$title = "MHCT Stats Tracker";
+
+require_once "common-header.php";
+print "<h4><a href='rh-tracker.php'>Relic Hunter tracking moved here.</a></h4><br/><br/>";
+require_once "stats.php";
+require_once "common-footer.php";
