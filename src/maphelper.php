727d24ea185ec1f45b98e4195dcefbc79473a693
acidRain810747 <acidRain810747@protonmail.com>
2025-05-07 15:18:28 +0300

    improve error handling

---
+++ src/maphelper.php
@@ -0,0 +1,28 @@
+<?php
+    $title = "MHCT Map Helper";
+//    $css = "styles/maphelper.css";
+    $js = "scripts/maphelper.js";
+    $fluid_container = true;
+    require_once "common-header.php";
+
+
+    require_once "searchByMouse.php";
+
+    $name = "mice";
+    echo '<div class="container-fluid" id="results">';
+    if (isset($_REQUEST['mice']) && !empty($_REQUEST['mice'])) {
+        require_once "helper-results-header.php";
+    }
+
+    if (isset($results['results']) && !empty($results['results'])) {
+        $column_titles = ["Attraction rate", "Attracted Hunts", "Total Hunts"];
+        $result_column_names = ["name" => "", "rate" => "&#37;", "attracted_hunts" => "", "total_hunts" => ""];
+        foreach ($results['results'] as $location_id => $result) {
+          require "helper-results-body.php";
+        }
+    }
+    echo '</div>';
+
+    require_once "helper-form.php";
+
+    require_once "common-footer.php";
