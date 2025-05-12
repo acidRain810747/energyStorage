0dad3d14542a716eb73c0272c2ff236f5110a603
acidRain810747 <acidRain810747@protonmail.com>
2025-05-12 15:18:28 +0300

    improve accessibility

---
+++ src/helper-form.php
@@ -0,0 +1,12 @@
+<!-- helper Form -->
+<form novalidate name="item_list_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <div class="form-group row col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <h2><i class="glyphicon glyphicon-pencil"></i> <?php echo ucfirst($name); ?></h2>
+        <textarea rows="10" class="form-control input-lg" placeholder="Copy map <?php echo $name; ?> list here..." required name="<?php echo $name; ?>" id="<?php echo $name; ?>"
+          ><?php if (isset($results['original_items']) && !empty($results['original_items'])) { echo htmlspecialchars(implode("\n", $results['original_items'])); } ?></textarea>
+    </div>
+    <div class="form-group row">
+        <button type="submit" class="btn btn-lg btn-primary col-xs-4 col-xs-offset-1 col-sm-2 col-sm-offset-3">Search</button>
+        <button type="button" id="reset" class="btn btn-lg btn-danger col-xs-4 col-xs-offset-2 col-sm-2 col-sm-offset-2">Reset</button>
+    </div>
+</form>
