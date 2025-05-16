e3e829200fb5a3f8bec82c0ad0a66688b94e0a56
acidRain810747 <acidRain810747@protonmail.com>
2025-05-16 15:18:28 +0300

    update package.json

---
+++ src/converter.php
@@ -0,0 +1,25 @@
+<?php
+    $title = "MHCT Converter";
+    // $css = "styles/loot.css";
+    $js = "scripts/converter.js";
+    $load_datatable_libraries = true;
+    require_once "common-header.php";
+?>
+
+        <input id="item_type" type="hidden" value="convertible">
+        <?php
+
+        if (!empty($_GET['item'])) {
+            print '<input id="prev_item" type="hidden" value="' . $_GET['item'] . '">';
+        } ?>
+
+        <div class="input-group col-sm-6 col-sm-offset-3">
+            <div class="input-group-addon"><strong>Convertible:</strong></div>
+            <input name="item" id="item" class="form-control input-lg" type="text" placeholder="Start typing convertible name and select." autofocus>
+            <div id="erase_item" class="input-group-addon fakebutton"><span class="glyphicon glyphicon-remove"></span></div>
+        </div>
+
+        <div id="results_total"></div>
+        <div id="results" class="table-responsive"></div>
+
+<?php require_once "common-footer.php"; ?>
